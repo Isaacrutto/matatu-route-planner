aa5cd3d0bbbbe28ae08769aae519d93d39606d50
root <root@DESKTOP-QHOOOTK.localdomain>
2025-03-27 09:00:00 +0300

    Fetched and displayed routes from JSON server

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch('http://localhost:3000/routes')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('routes-container');
+    data.forEach(route => {
+      const div = document.createElement('div');
+      div.classList.add('route-card');
+      div.innerHTML = `<h3>${route.origin} to ${route.destination}</h3><p>Fare: KES ${route.fare}</p>`;
+      container.appendChild(div);
+    });
+  });
